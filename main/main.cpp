98338843968a721456dad93077e6de9e9ed5115a
Andri Yadi <an.dri@me.com>
2017-12-30 10:37:40 +0700

    First working player

--- main/main.cpp
+++ main/main.cpp
@@ -1,30 +1,95 @@
-/*
- * 1. Open up the project properties
- * 2. Visit C/C++ General > Preprocessor Include Paths, Macros, etc
- * 3. Select the Providers tab
- * 4. Check the box for "CDT GCC Built-in Compiler Settings"
- * 5. Set the compiler spec command to "xtensa-esp32-elf-gcc ${FLAGS} -E -P -v -dD "${INPUTS}""
- * 6. Rebuild the index
-*/
 
-#include <esp_log.h>
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "sdkconfig.h"
-#include <Arduino.h>
+#include "Arduino.h"
 
 const static char *TAG ="APP";
 const static char *DEFAULT_WIFI_SSID = "DyWare-AP3";
 const static char *DEFAULT_WIFI_PASSWORD = "p@ssw0rd";
 
-#include <ESPectro32_Board.h>
+#define HTTP_DOWNLOAD_DEBUG_PRINT(...)  ESP_LOGI("HTTP", __VA_ARGS__);
 
-#include <WiFiManager.h>
-WiFiManager wifiMgr;
+static const uint8_t PROGMEM LED_PLAYING_SONG[] =
+{ B00011110,
+  B00110010,
+  B00100010,
+  B00100110,
+  B01101110,
+  B11101100,
+  B11000000
+};
 
-#include <ESPectro32_LedMatrix_Animation.h>
+#include "ESPectro32_Board.h"
+#include "WiFiManager.h"
+#include "ESPectro32_LedMatrix_Animation.h"
+#include "AudioPlayer.h"
 
 extern "C" {
-	#include <esp_request.h>
+	#include "esp_request.h"
+	void app_main(void);
+}
+
+
+WiFiManager wifiMgr;
+
+File downloadedFile;
+int download_callback(request_t *req, char *data, int len) {
+
+	req_list_t *found = req->response->header;
+	static int binary_len = -1, remain_len = -1;
+
+	static fs::FS fs = SD;
+
+	// HTTP_DOWNLOAD_DEBUG_PRINT("Response code: %d", req->response->status_code);
+
+	// while(found->next != NULL) {
+	// 	found = found->next;
+	// 	HTTP_DOWNLOAD_DEBUG_PRINT("Response header %s:%s", (char*)found->key, (char*)found->value);
+	// }
+
+	HTTP_DOWNLOAD_DEBUG_PRINT("Downloading...%d/%d bytes, remain=%d bytes", len, binary_len, remain_len);
+	if(req->response->status_code == 200) {
+		//First time
+		if(binary_len == -1) {
+			found = req_list_get_key(req->response->header, "Content-Length");
+			if(found) {
+				HTTP_DOWNLOAD_DEBUG_PRINT("Binary len=%s", (char*)found->value);
+				binary_len = atoi(found->value);
+				remain_len = binary_len;
+			} else {
+				HTTP_DOWNLOAD_DEBUG_PRINT("Error get content length");
+				return -1;
+			}
+
+			//Begin download
+			downloadedFile = fs.open("/WAV/DOWNLOAD.WAV", FILE_WRITE);
+			if (!downloadedFile) {
+				HTTP_DOWNLOAD_DEBUG_PRINT("Failed to open file for writing");
+				return -1;
+			}
+		}
+
+		//Downloading...
+		size_t written = downloadedFile.write((uint8_t *)data, len);
+		if (written == 0) {
+			HTTP_DOWNLOAD_DEBUG_PRINT("No data written to File");
+			downloadedFile.close();
+			return -1;
+		}
+
+		remain_len -= len;
+
+		//finish
+		if(remain_len == 0) {
+			HTTP_DOWNLOAD_DEBUG_PRINT("Download callback is finished");
+			downloadedFile.close();
+		}
+
+		return 0;
+	}
+
+	return -1;
 }
 
 void app_main(void)
@@ -33,21 +98,60 @@ void app_main(void)
 	Serial.begin(115200);
 
 	ESP_LOGD(TAG, "It begins!");
+	ESPectro32.begin();
 
+	//Explicitely begin SDCard operation
+	ESPectro32.SDCardBegin();
+	ESPectro32.SDCardPrintInfo();
+	ESPectro32.SDCardListDirectory();
+
+	//Preparing WiFiManager
 	ESPectro32_LedMatrix_Animation ledMatrixAnim = ESPectro32_LedMatrix_Animation::startWiFiAnimation();
 	wifiMgr.begin(WIFI_MODE_STA, false);
 	wifiMgr.connectToAP(DEFAULT_WIFI_SSID, DEFAULT_WIFI_PASSWORD);
 
-	//Actually start
+	//Actually start WiFiManager
 	wifiMgr.start();
 
 	if (wifiMgr.waitForConnection()) {
 		ledMatrixAnim.stop();
 		ESPectro32.LedMatrix().displayFrame(2);
 	}
-	
+
+	ESPectro32_LedMatrix_Animation downloadAnim = ESPectro32_LedMatrix_Animation::startDownloadAnimation();
+
+	request_t *req;
+	int status;
+	//req = req_new("http://music.albertarose.org/christmas/lyrics/music/AlvinAndTheChipmunks_WeWishYouAMerryChristmas.wav");
+	req = req_new("http://andriyadi.me/wp-content/uploads/2017/12/We_Wish_You_A_Merry_Christmas_The_Chipmunks_.wav");
+	//req->context = pvParameter;
+
+	req_setopt(req, REQ_SET_HEADER, "User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+	req_setopt(req, REQ_FUNC_DOWNLOAD_CB, download_callback);
+	status = req_perform(req);
+	req_clean(req);
+
+	if (status) {
+
+		downloadAnim.stop();
+		downloadAnim.clearFrames();
+
+		ESPectro32.LedMatrix().drawBitmapFull(LED_PLAYING_SONG);
+
+		HTTP_DOWNLOAD_DEBUG_PRINT("File is downloaded");
+
+		//Preparing AudioPlayer a.k.a WAVE Player
+		i2s_pin_config_t pin_config;
+		pin_config.bck_io_num = GPIO_NUM_26;
+		pin_config.ws_io_num = GPIO_NUM_25;
+		pin_config.data_out_num = GPIO_NUM_32;
+		pin_config.data_in_num = I2S_PIN_NO_CHANGE;
+
+		AudioPlayer *player = new AudioPlayer();
+		player->begin(SD, &pin_config);
+
+		player->playAsync("/WAV/DOWNLOAD.WAV");
+	}
 }
 
-extern "C" {
-	void app_main(void);
-}
+
